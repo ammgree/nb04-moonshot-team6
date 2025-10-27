fff607874d0b5f2611a2a63d82dc0d2cf77c9114
ammgree <kruhm5745@gmail.com>
2025-10-27 18:53:43 +0900

    fix: 라우터에 passport 인증 추가

--- src/routes/member.route.ts
+++ src/routes/member.route.ts
@@ -1,12 +1,33 @@
 import express from "express";
+import passport from "../configs/passport.js";
 import * as memberController from "../controllers/member.controller.js";
 
 const router = express.Router();
 router
-  .get("/projects/:projectId/users", memberController.getMembers)
-  .delete("/projects/:projectId/users/:userId", memberController.deleteMember)
-  .post("/projects/:projectId/invitations", memberController.inviteMember)
-  .post("/invitations/:invitationId/accept", memberController.acceptInvitation)
-  .delete("/invitations/:invitationId", memberController.cancelInvitation);
+  .get(
+    "/projects/:projectId/users",
+    passport.authenticate("jwt", { session: false }),
+    memberController.getMembers
+  )
+  .delete(
+    "/projects/:projectId/users/:userId",
+    passport.authenticate("jwt", { session: false }),
+    memberController.deleteMember
+  )
+  .post(
+    "/projects/:projectId/invitations",
+    passport.authenticate("jwt", { session: false }),
+    memberController.inviteMember
+  )
+  .post(
+    "/invitations/:invitationId/accept",
+    passport.authenticate("jwt", { session: false }),
+    memberController.acceptInvitation
+  )
+  .delete(
+    "/invitations/:invitationId",
+    passport.authenticate("jwt", { session: false }),
+    memberController.cancelInvitation
+  );
 
 export default router;
